2674d6d0ae59cb3ccf792bb6af4761bd8545d15f
Eugene Shevchenko <eugeneShevchenko0@gmail.com>
2024-01-04 23:55:12 +0500

    1, 2 lessons

---
+++ Notes/Models/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Models
+{
+    public class Game
+    {
+        public string? Name { get; set; }
+        public string? Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
