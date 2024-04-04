119a6a9de463585e841fe400b37335d493245cab
Eugene Shevchenko <eugeneShevchenko0@gmail.com>
2024-04-04 23:01:46 +0500

    Tags works somehow

---
+++ Notes_ViewModel/Helpers/TagSearchOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes_ViewModel.Helpers
+{
+	public enum TagSearchOptions
+	{
+		Notes,
+		Reminders,
+		Both
+	}
+}
